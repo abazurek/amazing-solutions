4270ddac9c9cd9fd52c93bca0a923708ff733bb0
abazurek <skala2294@gmail.com>
2020-11-27 14:29:10 +0100

    code apperance changes

--- tasks1/src/components/SearchComponent.tsx
+++ tasks1/src/components/SearchComponent.tsx
@@ -1,47 +1,50 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-import {delay} from "../service/delay";
+import { delay } from "../service/delay";
 
 import debounce from "../service/debounce";
 
+export default function SearchComponent({ searchSpaces, property }: any) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState();
+  const [error, setError] = useState();
 
-export default function SearchComponent({searchSpaces, property}:any) {
+  const debouncedSearchTerm = debounce(searchTerm, 500);
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState();
-    const [error, setError] = useState();
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      searchSpaces(debouncedSearchTerm)
+        .then(delay(500))
+        .then((data: string) => setResults(data))
+        .catch((err: any) => setError(<p>coś poszło nie tak...</p>));
+    } else {
+      setResults(null);
+      setError(null);
+    }
+  }, [debouncedSearchTerm]);
 
-    const debouncedSearchTerm = debounce(searchTerm, 500);
-
-    useEffect(()=>{
-        if(debouncedSearchTerm){
-            searchSpaces(debouncedSearchTerm)
-                .then(delay(500))
-                .then((data:string)=>setResults(data))
-                .catch((err:any)=>setError(<p>coś poszło nie tak...</p>));
-        }else {
-            setResults(null);
-            setError(null)
-        }
-
-    }, [debouncedSearchTerm])
-
-    return (
-        <section>
-            <form>
-                <label>
-                    <input type='text' placeholder='write the text..'
-                           onChange={({target}) => setSearchTerm(target.value)}/>
-                </label>
-            </form>
-            <ul>
-                {results ? results[property].map(function (item: any) {
-                        return <li key={results[property].indexOf(item)}>{Object.values(item)}</li>
-                    })
-
-                    : error
-                }
-            </ul>
-        </section>
-    )
+  return (
+    <section>
+      <form>
+        <label>
+          <input
+            type="text"
+            placeholder="write the text.."
+            onChange={({ target }) => setSearchTerm(target.value)}
+          />
+        </label>
+      </form>
+      <ul>
+        {results
+          ? results[property].map(function (item: any) {
+              return (
+                <li key={results[property].indexOf(item)}>
+                  {Object.values(item)}
+                </li>
+              );
+            })
+          : error}
+      </ul>
+    </section>
+  );
 }
--- tasks1/src/delay.test.tsx
+++ tasks1/src/delay.test.tsx
@@ -1,16 +1,18 @@
-import {delay} from "./service/delay";
+import { delay } from "./service/delay";
 
-type Results={};
+type Results = {};
 
-const promise = (num1: number, num2:number): Promise<Results> => {
-    return new Promise((res, rej) => {
-        const result:number=num1+num2
-      res(result)
-        rej("CatchError")
-    });
-}
+const promise = (num1: number): Promise<Results> => {
+  return new Promise((res, rej) => {
+    res(num1);
+    rej("CatchError");
+  });
+};
 
-test('get result after delay', done => {
-    promise(10, 50).then(delay(700)).then((result)=>expect(result).toEqual(60)).catch(err=>console.log(err));
-    done();
-})
+test("get result after delay", (done) => {
+  promise(10)
+    .then(delay(700))
+    .then((result) => expect(result).toEqual(60))
+    .catch((err) => console.log(err));
+  done();
+});
